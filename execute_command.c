1a004ca778bc0d385d053cb55f5089527ba6aaf8
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-24 18:05:55 +0100

    update

--- execute_command.c
+++ execute_command.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-#include <libgen.h>
 
 /**
  * execute_command - a function that execute a command
@@ -9,7 +8,6 @@
  */
 void execute_command(const char *program_name, const char *command)
 {
-	int status;
 	pid_t pid = fork();
 
 	if (pid < 0)
@@ -20,34 +18,21 @@ void execute_command(const char *program_name, const char *command)
 	else if (pid == 0)
 	{
 		/* Child process */
-		char *envp[] = {NULL}; /* Empty environment */
+		char *envp[] = {NULL};
 		char *args[2];
 
 		args[0] = strdup(command);
 		args[1] = NULL;
 		execve(command, args, envp);
 
-		/* free the memory allocated by strdup */
 		free(args[0]);
-
-		/* printf error message withe program's name */
 		perror(program_name);
-
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		/* Parent process */
-		wait(&status);
-		if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
-		{
-			char *executable = strdup(command);
-			char *base = basename(executable);
-
-			fprintf(stderr, "%s: %s: %s\n", program_name, base, "command not found");
-
-			free(executable);
-		}
+		wait(NULL);
 	}
 }
 
