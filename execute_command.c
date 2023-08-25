879ad5527943fa3f2934a9db4897055cc8c076e0
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-25 15:41:29 +0100

    fork must not be called if the command doesn’t exist in the execute_command function

--- execute_command.c
+++ execute_command.c
@@ -9,8 +9,14 @@
  */
 void execute_command(const char *prg_name, char *const args[], int cmd_num)
 {
-	pid_t pid = fork();
+	pid_t pid;
 
+	if (access(args[0], X_OK) == -1)
+	{
+		fprintf(stderr, "%s: %d: %s: not found\n", prg_name, cmd_num, args[0]);
+		return;
+	}
+	pid = fork();
 	if (pid < 0)
 	{
 		perror("Fork failed");
