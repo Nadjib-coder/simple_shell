d341b3fe51ba3e7a1b3f050bf3b2ceebc3a8129c
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-08-25 18:05:50 +0100

    update execute_command.c

--- execute_command.c
+++ execute_command.c
@@ -9,6 +9,7 @@
  */
 void execute_command(const char *prg_name, char *const args[], int cmd_num)
 {
+	/*
 	pid_t pid;
 
 	if (access(args[0], X_OK) == 0)
@@ -20,6 +21,8 @@ void execute_command(const char *prg_name, char *const args[], int cmd_num)
 		fprintf(stderr, "%s: %d: %s: not found\n", prg_name, cmd_num, args[0]);
 		return;
 	}
+	*/
+	pid_t pid = fork();
 	if (pid < 0)
 	{
 		perror("Fork failed");
--- shell.h
+++ shell.h
@@ -12,5 +12,7 @@
 /* function declaration */
 void split_input(char *input, char *cmd_args[]);
 void execute_command(const char *prg_name, char *const args[], int cmd_num);
+char *find_command(const char *command);
+int execute_full_path(const char *command, char *const args[]);
 
 #endif
